4f9aeaa03f4adf9031ef3fbb1f64f26ad92ab78d
ArielAVega <avega@fi.unju.edu.ar>
2025-09-07 08:47:54 -0300

    Implement Player.cs with arrows movement and test it in Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+}
